3f643ad3cf0ee07575dbb24751b53fa452ea22b1
DanielaCruz <danycc26@gmail.com>
2015-12-08 15:31:05 -0500

    vista representante empresarial

---
+++ SIPA/vista/js/representanteEmpresaril.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    perfil();
+
+    $('#perfil').on('click', function () {
+        perfil();
+    });
+
+    $('#practica').on('click', function () {
+        $('#contenido').load('vista/html/docente_practicas.html');
+    });
+
+    $('#comentarios').on('click', function () {
+
+    });
+
+    $('#reportes').on('click', function () {
+
+    });
+
+    $('#cambiarContrasena').on('click',function(){
+       $('#contenido').load('vista/html/cambioClave.html');
+   });
+
+    $("#cerrarSesion").on('click', function () {
+        $.post("controlador/fachada.php", {
+            clase: 'Usuario',
+            oper: 'cerrarSesion'
+        }, function (data) {
+            location.reload();
+        }, 'json');
+    });
+
+    function perfil() {
+        $.post("controlador/fachada.php", {
+            clase: 'Usuario',
+            oper: 'perfil',
+        }, function (data) {
+            //console.log(data);
+            $("#contenido").html(data);
+        }, 'json');
+    }
+});
